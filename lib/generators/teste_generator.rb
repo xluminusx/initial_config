0ac339801ef634a3bf88dfe42afad9cd3e7f259d
Pedro Coelho <pedro.coelho@sefin.ro.gov.br>
2022-02-15 10:39:33 -0400

    teste

--- lib/generators/teste_generator.rb
+++ lib/generators/teste_generator.rb
@@ -4,6 +4,8 @@ class TesteGenerator < Rails::Generators::Base
   argument :params, :type => :hash, :default => {}
 
   def init
+    @yarns = ['popper.js@1.16.1', 'acorn@8.7.0', 'jquery@3.6.0', 'bootstrap@4.6.0', 'bootstrap-icons@1.5.0', 'icheck-bootstrap@3.0.1']
+    @kaminaris = ['first_page', 'gap', 'last_page', 'next_page', 'page', 'paginator', 'prev_page']
   end
 
   def exec
@@ -15,19 +17,12 @@ class TesteGenerator < Rails::Generators::Base
   private
 
   def yarn_camp
-    yarns = ['popper.js@1.16.1', 'acorn@8.7.0', 'jquery@3.6.0', 'bootstrap@4.6.0', 'bootstrap-icons@1.5.0', 'icheck-bootstrap@3.0.1']
     puts ''
     puts "========================= YARN PACKAGES ========================="
     puts "================================================================="
-    yarns.each do |y|
+    @yarns.each do |y|
       puts %x(yarn add #{y})
     end
-    # puts %x(yarn add popper.js@1.16.1)
-    # puts %x(yarn add acorn@8.7.0)
-    # puts %x(yarn add jquery@3.6.0)
-    # puts %x(yarn add bootstrap@4.6.0)
-    # puts %x(yarn add bootstrap-icons@1.5.0)
-    # puts %x(yarn add icheck-bootstrap@3.0.1)
     puts "================================================================="
   end
   
@@ -46,12 +41,10 @@ class TesteGenerator < Rails::Generators::Base
     puts "================================================================"
     puts %x(rails generate devise:install)
     puts %x(rails generate devise user)
-    puts %x(rails db:migrate)
     puts "================================================================"
   end
 
   def arquivo_camp
-    kaminaris = ['first_page', 'gap', 'last_page', 'next_page', 'page', 'paginator', 'prev_page']
     puts ''
     puts "=========================== ARCHIVES ==========================="
     puts "================================================================"
@@ -67,20 +60,10 @@ class TesteGenerator < Rails::Generators::Base
       template  'mvc/controller.rb'     , 'app/controllers/dados_controller.rb'    , force: true
       copy_file 'mvc/view.rb'           , 'app/views/initial_config/index.html.erb', force: true
       
-      kaminaris.each do |k|
+      @kaminaris.each do |k|
         copy_file "kaminari/_#{k}.html.erb", "app/views/kaminari/_#{k}.html.erb" , force: true
       end
 
-      # copy_file 'kaminari/first.rb'     , 'app/views/kaminari/_first_page.html.erb', force: true 
-      # copy_file 'kaminari/gap.rb'       , 'app/views/kaminari/_gap.html.erb'       , force: true
-      # copy_file 'kaminari/last.rb'      , 'app/views/kaminari/_last_page.html.erb' , force: true
-      # copy_file 'kaminari/next.rb'      , 'app/views/kaminari/_next_page.html.erb' , force: true
-      # copy_file 'kaminari/page.rb'      , 'app/views/kaminari/_page.html.erb'      , force: true
-      # copy_file 'kaminari/paginator.rb' , 'app/views/kaminari/_paginator.html.erb' , force: true
-      # copy_file 'kaminari/prev.rb'      , 'app/views/kaminari/_prev_page.html.erb' , force: true
-
-      # copy_file 'devise/migrate.rb'         , "db/migrate/#{(m[0..13].to_i + 1).to_s}_devise_create_users.rb"
-      # copy_file 'devise/model_user.rb'                                          , 'app/models/user.rb'
       copy_file 'devise/devise.js', 'app/javascript/packs/devise.js'
       copy_file 'devise/config_devise.rb', 'config/initializers/devise.rb', force: true
       copy_file 'devise/users_controller.rb'                                    , 'app/controllers/users_controller.rb'
@@ -90,17 +73,19 @@ class TesteGenerator < Rails::Generators::Base
       copy_file 'devise/views/users/mailer/email_changed.html.erb'              , 'app/views/users/mailer/email_changed.html.erb'
       copy_file 'devise/views/users/mailer/password_change.html.erb'            , 'app/views/users/mailer/password_change.html.erb'
       copy_file 'devise/views/users/mailer/reset_password_instructions.html.erb', 'app/views/users/mailer/reset_password_instructions.html.erb'
-      copy_file 'devise/views/users/mailer/unlock_instructions.html.erb', 'app/views/users/mailer/unlock_instructions.html.erb'
-      copy_file 'devise/views/users/passwords/edit.html.erb', 'app/views/users/passwords/edit.html.erb'
-      copy_file 'devise/views/users/passwords/new.html.erb', 'app/views/users/passwords/new.html.erb'
-      copy_file 'devise/views/users/registrations/edit.html.erb', 'app/views/users/registrations/edit.html.erb'
-      copy_file 'devise/views/users/registrations/new.html.erb', 'app/views/users/registrations/new.html.erb'
-      copy_file 'devise/views/users/sessions/new.html.erb', 'app/views/users/sessions/new.html.erb'
-      copy_file 'devise/views/users/shared/_error_messages.html.erb', 'app/views/users/shared/_error_messages.html.erb'
-      copy_file 'devise/views/users/shared/_links.html.erb', 'app/views/users/shared/_links.html.erb'
-      copy_file 'devise/views/users/unlocks/new.html.erb', 'app/views/users/unlocks/new.html.erb'
+      copy_file 'devise/views/users/mailer/unlock_instructions.html.erb'        , 'app/views/users/mailer/unlock_instructions.html.erb'
+      copy_file 'devise/views/users/passwords/edit.html.erb'                    , 'app/views/users/passwords/edit.html.erb'
+      copy_file 'devise/views/users/passwords/new.html.erb'                     , 'app/views/users/passwords/new.html.erb'
+      copy_file 'devise/views/users/registrations/edit.html.erb'                , 'app/views/users/registrations/edit.html.erb'
+      copy_file 'devise/views/users/registrations/new.html.erb'                 , 'app/views/users/registrations/new.html.erb'
+      copy_file 'devise/views/users/sessions/new.html.erb'                      , 'app/views/users/sessions/new.html.erb'
+      copy_file 'devise/views/users/shared/_error_messages.html.erb'            , 'app/views/users/shared/_error_messages.html.erb'
+      copy_file 'devise/views/users/shared/_links.html.erb'                     , 'app/views/users/shared/_links.html.erb'
+      copy_file 'devise/views/users/unlocks/new.html.erb'                       , 'app/views/users/unlocks/new.html.erb'
 
       copy_file 'routes.rb', 'config/routes.rb', force: true
+
+      puts %x(rails db:migrate)
             
     puts "================================================================"
   end
